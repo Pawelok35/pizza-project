540e6bb9dcf9dfe2bccc235fbf827635de050ccf
pawelok <pawelok35@gmail.com>
2023-07-14 19:04:18 +0200

    Update processOrder & remove active class from all pizza params

--- src/js/script.js
+++ src/js/script.js
@@ -63,10 +63,10 @@
 
       thisProduct.renderInMenu();
       thisProduct.getElements();
-      thisProduct.initAccordion();
+      thisProduct.initAccordion(); 
       thisProduct.initOrderForm();
       thisProduct.processOrder();
-      // console.log('new Product:', thisProduct);
+     // console.log('new Product:', thisProduct);
     }
 
     renderInMenu() {
@@ -124,49 +124,67 @@
     initOrderForm() {
       const thisProduct = this;
       //console.log(thisProduct);
-      thisProduct.form.addEventListener('submit', function (event) {
+      thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
         thisProduct.processOrder();
       });
-
-      for (let input of thisProduct.formInputs) {
-        input.addEventListener('change', function () {
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
           thisProduct.processOrder();
         });
       }
-
-      thisProduct.cartButton.addEventListener('click', function (event) {
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
       });
+      
+    
+      
     }
     processOrder() {
       const thisProduct = this;
-
+      //console.log(thisProduct);
+    
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData', formData);
-
+    
+      // set price to default price
       let price = thisProduct.data.price;
-
-      for (let paramId in thisProduct.data.params) {
-        const param = thisProduct.data.params[paramId];   // -> optionId will be array ? 
-
-        for (let optionId in param.options) {
-          const option = param.option[optionId];
+    
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        //console.log(paramId, param);
+    
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
           console.log(optionId, option);
-
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+        
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
             if (!option.default) {
               price += option.price;
+              // add option price to price variable
             }
           } else {
+            // check if the option is default
             if (option.default == true) {
-              price -= option.price;
+             price -= option.price;
+              // reduce price variable
             }
           }
+        
         }
+        
       }
-
+    
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
